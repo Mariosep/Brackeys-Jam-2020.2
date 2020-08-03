8267d4463a36c1adeef9b98af6a28de28343b64e
adrianqm <37654854+adrianqm@users.noreply.github.com>
2020-08-03 16:41:36 +0200

    First commit adri

---
+++ Assets/Scenes/Adrián test_Profiles/PostProcessingRewind.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+public class PostProcessingRewind : MonoBehaviour
+{
+    public PostProcessVolume volume;
+
+    private ColorGrading colorGrading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume.profile.TryGetSettings(out colorGrading);
+        colorGrading.saturation.value = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)){
+            colorGrading.saturation.value = -100f;
+        }else{
+            colorGrading.saturation.value = 0.0f;
+        }
+    }
+}
